f952bf8700b7d5a16d860e4db8146e737aa16609
vitaliasi <Silvestfzu1992>
2019-09-02 16:16:38 +0300

    commit 2

--- src/main/java/browsers/Driver.java
+++ src/main/java/browsers/Driver.java
@@ -2,29 +2,43 @@ package browsers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    private static WebDriver driver = new ChromeDriver();
-    //TODO add edge and firefox
 
     protected static WebDriver getDriver() {
+        String os = System.getProperty("os.name").toLowerCase();
+        WebDriver driver = new ChromeDriver();
+        if (os.contains("mac")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
+        } else {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("C:\\Users\\vitalija.silvestruk\\IdeaProjects\\ddf-xls\\src\\test\\resources\\chromedriver.exe"));
+//            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+        }
+//        System.setProperty("webdriver.chrome.driver", "C:\Users\Ampacattu\Downloads\chromedriver_win32\chromedriver.exe");
         return driver;
     }
 
+
+//    private static WebDriver driver = new ChromeDriver();
+    //TODO add edge and firefox
+
+//    protected static WebDriver getDriver() {
+//        return driver;
+//    }
+
     protected static void open(String url) {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(url);
-        driver.manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().get(url);
+        getDriver().manage().window().maximize();
     }
 
     public static void close() {
-        driver.close();
+        getDriver().close();
     }
 
     public static void quit() {
-        driver.quit();
+        getDriver().quit();
     }
 }
